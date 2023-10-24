bcbacd246a348f10ae15e247e23ec2a66c1c1df1
Bekbol Askerov <askerov.bekbol@inbox.ru>
2023-10-24 19:53:36 +0600

    add jwt dependencies

--- src/main/java/com/pet/healthwave/user/UserRepository.java
+++ src/main/java/com/pet/healthwave/user/UserRepository.java
@@ -3,11 +3,13 @@ package com.pet.healthwave.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User WHERE u.email = :email")
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findByUsername(@Param("email") String email);
 
 }
