1549fd8a5b35f2b15c58d09923894b969d552e55
Bekbol Askerov <askerov.bekbol@inbox.ru>
2023-10-24 19:43:44 +0600

    add user repository and method to find by username

--- src/main/java/com/pet/healthwave/user/UserRepository.java
+++ src/main/java/com/pet/healthwave/user/UserRepository.java
@@ -1,8 +1,13 @@
 package com.pet.healthwave.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Query("SELECT u FROM User WHERE u.email = :email")
+    Optional<User> findByUsername(@Param("email") String email);
 
 }
