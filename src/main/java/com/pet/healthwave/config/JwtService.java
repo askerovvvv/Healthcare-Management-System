694731969bd83564df1e3a26f260511891e294c5
Bekbol Askerov <askerov.bekbol@inbox.ru>
2023-10-24 20:16:04 +0600

    add jwtauth service

--- src/main/java/com/pet/healthwave/config/JwtService.java
+++ src/main/java/com/pet/healthwave/config/JwtService.java
@@ -59,4 +59,16 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 }
