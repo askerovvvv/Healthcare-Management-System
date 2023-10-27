0a8f3c09ad24684b980002d86e3c1a7773440952
Bekbol Askerov <askerov.bekbol@inbox.ru>
2023-10-27 11:39:46 +0600

    add validator doc

--- src/main/java/com/pet/healthwave/validator/CustomValidationError.java
+++ src/main/java/com/pet/healthwave/validator/CustomValidationError.java
@@ -1,5 +1,10 @@
 package com.pet.healthwave.validator;
 
+/**
+ * If validation has error, program will save information in this fields
+ * @param field field that was wrong
+ * @param message description of error
+ */
 public record CustomValidationError (
         String field,
         String message
--- src/main/java/com/pet/healthwave/validator/DefaultValidatorImpl.java
+++ src/main/java/com/pet/healthwave/validator/DefaultValidatorImpl.java
@@ -8,12 +8,28 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+
+/**
+ * This class validates fields when user sends request. In necessary request classes(where defined fields for request)
+ * there are some annotations list @NotNull and this validator checks if everything is good.
+ * It has generic<T> to validate multiple objects.
+ *
+ * @author askerovvvv
+ */
+
+
 @RequiredArgsConstructor
 public class DefaultValidatorImpl<T> implements DefaultValidator<T> {
 
     private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     private final Validator validator = factory.getValidator();
 
+    /**
+     * this method validates and if something is wrong saves it in to set.
+     *
+     * @param objectToValidate object of class that need to be validated
+     * @return List of validation errors(field: message)
+     */
     @Override
     public List<CustomValidationError> validate(T objectToValidate) {
         Set<ConstraintViolation<T>> violations = validator.validate(objectToValidate);
