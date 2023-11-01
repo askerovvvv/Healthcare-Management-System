5b3cd50cc6d1381be561dfdbecb180d17b99c891
Bekbol Askerov <askerov.bekbol@inbox.ru>
2023-11-01 16:13:06 +0600

    add new method in user repository

--- src/main/java/com/pet/healthwave/user/UserRepository.java
+++ src/main/java/com/pet/healthwave/user/UserRepository.java
@@ -1,6 +1,8 @@
 package com.pet.healthwave.user;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +11,14 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.email = ?1")
-    Optional<User> findByUsername(@Param("email") String email);
 
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findByUsername(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u " +
+            "SET u.emailVerified = TRUE " +
+            "WHERE u.email = ?1")
+    int updateEmailVerified(String email);
 }
