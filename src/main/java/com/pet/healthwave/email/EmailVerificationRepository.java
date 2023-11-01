d7c3b66eaaa4187d14ee024a983ce36b283866c3
Bekbol Askerov <askerov.bekbol@inbox.ru>
2023-11-01 16:13:30 +0600

    add new method in email repository

--- src/main/java/com/pet/healthwave/email/EmailVerificationRepository.java
+++ src/main/java/com/pet/healthwave/email/EmailVerificationRepository.java
@@ -1,15 +1,24 @@
 package com.pet.healthwave.email;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface EmailVerificationRepository extends JpaRepository<EmailVerificationToken, Long> {
     @Query("SELECT e FROM EmailVerificationToken e WHERE e.token = ?1")
-    Optional<EmailVerificationToken> findByToken(@Param("email") String token);
+    Optional<EmailVerificationToken> findByToken(String token);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE EmailVerificationToken e " +
+            "SET e.confirmedAt = ?1 " +
+            "WHERE e.token = ?2" )
+    int updateConfirmedAt(LocalDateTime confirmedAt, String token);
 }
