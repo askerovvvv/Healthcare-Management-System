11d9b72152ce4d914db888af984bff8a623fd340
Bekbol Askerov <askerov.bekbol@inbox.ru>
2023-11-01 17:43:46 +0600

    handle new exceptions

--- src/main/java/com/pet/healthwave/handler/DefaultExceptionHandler.java
+++ src/main/java/com/pet/healthwave/handler/DefaultExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.pet.healthwave.handler;
 
-import com.pet.healthwave.validator.ValidationException;
+import com.pet.healthwave.exceptions.AuthException;
+import com.pet.healthwave.exceptions.DefaultValidationException;
+import com.pet.healthwave.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,19 +10,44 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class DefaultExceptionHandler {
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<?> handleException(ValidationException exception, HttpServletRequest request) {
+    @ExceptionHandler(DefaultValidationException.class)
+    public ResponseEntity<?> handleException(DefaultValidationException exception, HttpServletRequest request) {
+        Map<String, Object> apiError = new HashMap<>();
+        apiError.put("path", request.getRequestURI());
+        apiError.put("message", exception.getMessage());
+        apiError.put("localDateTime", LocalDateTime.now());
+        apiError.put("status", HttpStatus.BAD_REQUEST.value());
+        apiError.put("errors", exception.getErrors());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ApiError> handleException(AuthException exception, HttpServletRequest request) {
         ApiError apiError = new ApiError(
                 request.getRequestURI(),
                 exception.getMessage(),
                 HttpStatus.BAD_REQUEST.value(),
                 LocalDateTime.now()
         );
-        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<ApiError> handleException(ObjectNotFoundException exception, HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
     }
 
 }
